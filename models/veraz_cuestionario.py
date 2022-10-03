f436373dc5ee55cb8626e046700046a0b0d7ecbc
Levis <levislibra@hotmail.com>
2022-10-03 16:48:35 +0000

    Update

--- models/veraz_cuestionario.py
+++ models/veraz_cuestionario.py
@@ -6,6 +6,8 @@ import requests
 
 ENDPOINT_VERAZ_VID = 'https://api-idvalidator.equifax.com.ar/v1/s2s/clientID/IDCLEXP/validation/full'
 
+ENDPOINT_VERAZ_VID_ANSWERS = 'https://api-idvalidator.equifax.com.ar/v1/s2s/clientID/IDCLEXP/validation/questionnaire/answers'
+
 class FinancieraVerazCuestionario(models.Model):
 	_name = 'financiera.veraz.cuestionario'
 	
@@ -22,10 +24,14 @@ class FinancieraVerazCuestionario(models.Model):
 	pregunta_4_id = fields.Many2one('financiera.veraz.cuestionario.pregunta', 'Pregunta 4')
 	pregunta_5_id = fields.Many2one('financiera.veraz.cuestionario.pregunta', 'Pregunta 5')
 	pregunta_actual_id = fields.Many2one('financiera.veraz.cuestionario.pregunta', 'Pregunta actual', compute='_compute_pregunta_actual_id')
+	pregunta_actual_texto = fields.Char('Pregunta actual', compute='_compute_pregunta_actual_texto')
 	# respuestas_test = fields.Char('Respuestas test')
 	# cuestionario = fields.Char('Respuestas', compute='_compute_cuestionario')
 	state = fields.Selection([('pendiente', 'Pendiente'), ('rechazado', 'Rechazado'), ('aprobado', 'Aprobado')], string='Estado', default='pendiente')
-	state_pregunta = fields.Selection([('inicial', 'Inicial'), ('pregunta_1', 'Pregunta 1'), ('pregunta_2', 'Pregunta 2'), ('pregunta_3', 'Pregunta 3'), ('pregunta_4', 'Pregunta 4'), ('pregunta_5', 'Pregunta 5')], string='Estado pregunta', default='inicial')
+	state_pregunta = fields.Selection([('inicial', 'Inicial'), ('pregunta_1', 'Pregunta 1'), ('pregunta_2', 'Pregunta 2'), ('pregunta_3', 'Pregunta 3'), ('pregunta_4', 'Pregunta 4'), ('pregunta_5', 'Pregunta 5'),('finalizado', 'Finalizado')], string='Estado pregunta', default='inicial')
+	id_cuestionario_generado = fields.Char('Cuestionario generado ID')
+	id_transaccion = fields.Char('Transacción ID')
+	transaction_state_description = fields.Char('Estado de transaccion')
 	score = fields.Integer('Score')
 	company_id = fields.Many2one('res.company', 'Empresa', required=False, default=lambda self: self.env['res.company']._company_default_get('financiera.veraz.cuestionario'))
 	
@@ -62,13 +68,16 @@ class FinancieraVerazCuestionario(models.Model):
 			json=params,
 			headers=headers)
 		data = response.json()
-		print('data: ', data)
+		# print('data: ', data)
 		if response.status_code != 200 or ('errors' in data and data['errors']):
 			raise ValidationError("Error en la obtencion del cuestionario Veraz: " + data['errors'][0]['message'])
 		else:
+			self.id_cuestionario_generado = data['payload']
+			self.id_transaccion = data['payload']['idTransaccion']
 			cuestionario = data['payload']['questionnaire']
-			print('cuestionario', cuestionario)
+			print('cuestionario id', cuestionario['id'])
 			self.name_cuestionario = cuestionario['name']
+			self.id_cuestionario_generado = cuestionario['id']
 			# print('questionnaire', cuestionario)
 			for pregunta in cuestionario['questionsOfGeneratedQuestionnaire']:
 				print('pregunta', pregunta)
@@ -138,6 +147,64 @@ class FinancieraVerazCuestionario(models.Model):
 		elif self.state_pregunta == 'pregunta_5':
 			self.pregunta_actual_id = self.pregunta_5_id
 
+	@api.one
+	def _compute_pregunta_actual_texto(self):
+		self.pregunta_actual_texto = ''
+		if self.pregunta_actual_id:
+			self.pregunta_actual_texto = self.pregunta_actual_id.texto
+
+	@api.one
+	def siguiente_pregunta(self):
+		if self.state_pregunta == 'pregunta_1':
+			self.state_pregunta = 'pregunta_2'
+		elif self.state_pregunta == 'pregunta_2':
+			self.state_pregunta = 'pregunta_3'
+		elif self.state_pregunta == 'pregunta_3':
+			self.state_pregunta = 'pregunta_4'
+		elif self.state_pregunta == 'pregunta_4':
+			self.state_pregunta = 'pregunta_5'
+		elif self.state_pregunta == 'pregunta_5':
+			self.evaluar_cuestionario()
+			self.state_pregunta = 'finalizado'
+
+	def get_dict_respuestas(self):
+		print('_get_dict_respuestas')
+		respuestas_dict = []
+		for pregunta_id in self.pregunta_ids:
+			respuestas_dict.append({
+				'idQuestion': pregunta_id.id_pregunta,
+				'idOption': pregunta_id.opcion_ids[pregunta_id.id_respuesta].id_opcion,
+			})
+		print('respuestas_dict', respuestas_dict)
+		return respuestas_dict
+
+
+	@api.one
+	def evaluar_cuestionario(self):
+		ret = False
+		veraz_configuracion_id = self.company_id.veraz_configuracion_id
+		headers = {
+			'Authorization': 'Bearer ' + veraz_configuracion_id.get_token_veraz_idvalidator(),
+			'Content-Type': 'application/json'
+		}
+		params = {
+			'idQuestionnaireGenerated': self.id_cuestionario_generado,
+			'idTransaction': self.id_transaccion,
+			'questionnaireResponse': self.get_dict_respuestas(),
+		}
+		print('params', params)
+		response = requests.post(
+			ENDPOINT_VERAZ_VID_ANSWERS,
+			json=params,
+			headers=headers)
+		data = response.json()
+		print('data: ', data)
+		if response.status_code != 200 or ('errors' in data and data['errors']):
+			raise ValidationError("Error en la obtencion del cuestionario Veraz: " + data['errors'][0]['message'])
+		else:
+			self.score = data['payload']['score']
+			self.transaction_state_description = data['payload']['transactionStateDescription']
+
 class FinancieraVerazCuestionarioPregunta(models.Model):
 	_name = 'financiera.veraz.cuestionario.pregunta'
 	
@@ -162,4 +229,4 @@ class FinancieraVerazCuestionarioPreguntaOpcion(models.Model):
 		if self.pregunta_id.id_respuesta >= 0:
 			self.pregunta_id.opcion_ids[self.pregunta_id.id_respuesta].respuesta = False
 		self.respuesta = True
-		self.pregunta_id.id_respuesta = self.id_opcion
+		self.pregunta_id.id_respuesta = self.id_opcion - 1
